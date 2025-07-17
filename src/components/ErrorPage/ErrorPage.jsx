702beff8e0a24f5a5ef410849d3b57ec88be0e30
Yousuf7900 <yousufislam7900@gmail.com>
2025-07-18 00:52:01 +0600

    first commit

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+    return (
+        <div className="text-center">
+            <h2 className="text-4xl font-semibold mt-4">Page not found</h2>
+            <p>Status: 404</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
